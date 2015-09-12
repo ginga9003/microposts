c71cc5c6bd92911f3c14a13ca8e06f6a8efde456
ginga9003 <ginga9003@gmail.com>
2015-09-12 15:51:50 +0000

    repair bugs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,11 @@ class StaticPagesController < ApplicationController
   def home
     # ログインしてたら全投稿を取得する
     if logged_in? 
-      @microposts = current_user.microposts.build
-      
       @following_count = current_user.following_users.length # フォロー数
       @follower_count = current_user.follower_users.length # フォロワー数
       @post_count = current_user.microposts.length # 投稿数
+
+      @microposts = current_user.microposts.build
     end
   end
 end
